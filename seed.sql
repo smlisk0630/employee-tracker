b2c4c735779ff295d01995a5f6260213c11481e4
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-02-26 08:51:42 -0500

    Updated seed file

--- seed.sql
+++ seed.sql
@@ -1,9 +1,9 @@
 USE employee_trackerDB;
 -- Data for department table
-INSERT INTO department (name) values ("Sales"), ("Legal"), ("IT"), ("Finance"), ("Marketing"), ("Engineering");
+INSERT INTO department (id, name) values (1, "Sales"), (2, "Legal"), (3, "IT"), (4, "Finance"), (5, "Marketing"), (6, "Engineering");
 
 -- Data for role table
-INSERT INTO role (title, salary) values ("Salesperson", 80000), ("Lawyer", 190000), ("Software Engineer", 120000), ("Accountant", 125000), ("Manager", 100000), ("Administrator", 90000);
+INSERT INTO role (id, title, salary, department_id) values (1, "Salesperson", 80000, 1), (2, "Lawyer", 190000, 2), (3, "Software Engineer", 120000, 6), (4, "Accountant", 125000, 4), (5, "Manager", 100000, 1), (6, "Administrator", 90000, 3);
 
 -- Data for employee table
-INSERT INTO employee (first_name, last_name) values ("John", "Doe"), ("Ashley", "Chan"), ("Mike", "Rodriguez"), ("Kevin", "Tupik"), ("Malia", "Brown"), ("Sarah", "Lourd"), ("Tom", "Allen")
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) values (1, "John", "Doe", 1, 5), (2, "Ashley", "Chan", 5), (3, "Mike", "Rodriguez", 1, 5), (4, "Kevin", "Tupik", 6, 6), (5, "Malia", "Brown", 4), (6, "Sarah", "Lourd", 2), (7, "Tom", "Allen", 2)
